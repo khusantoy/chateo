c49a2edbccdc7b7d92f16c0d68b3ffaa917b03db
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-07-08 09:40:50 +0500

    added push notification

--- lib/services/firebase_push_notification_service.dart
+++ lib/services/firebase_push_notification_service.dart
@@ -10,6 +10,20 @@ class FirebasePushNotificationService {
     // qurilma TEKEN'nini olamiz, shu orqli qaysi qurilmaga xabarnoma yuborishni aniqlaymiz
     final token = await _pushNotification.getToken();
 
-    print(token);
+    // backgroundda xabar kelsa ishlaydi
+    FirebaseMessaging.onMessage.listen((message) {
+      print("XABAR ORQALI DASTURNI OCHGANDA KELDI");
+      print("Xabar: $message");
+    });
+
+    // foregroundda xabar kelsa ishlaydi
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("DASTURDA BO'LMAGANDA XABAR KELDI");
+      print('XABAR: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
   }
 }
